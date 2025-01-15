3a6cc882e1a718aee33fd5172c4993c6dfa95cdd
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-16 01:40:58 +0530

    mvp

--- backend/src/middlewares/corsMiddleware.js
+++ backend/src/middlewares/corsMiddleware.js
@@ -3,7 +3,7 @@ import cors from "cors";
 export const corsOptions = {
   origin: process.env.CLIENT_URL || "https://quingo-d3dd.vercel.app/", // Replace with your frontend URL
   credentials: true,
-  methods: 'GET,POST,OPTIONS',
+  methods: ['GET','POST','OPTIONS'],
   allowedHeaders: 'Content-Type, Authorization',
 };
 
