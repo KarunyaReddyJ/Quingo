4d5a4fc154e4d4b2236f1c0ad8e6e3b7249f36a4
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-15 23:10:25 +0530

    mvp

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,14 @@ import logger from "../src/middlewares/loggerMiddleware.js";
 import { useCors } from "../src/middlewares/corsMiddleware.js";
 import postRoutes from "../src/routes/postRoutes.js";
 import userRoutes from "../src/routes/userRoutes.js";
+import dotenv from "dotenv";
+import connectDB from "../src/config/db.js";
 // import adminRoutes from "./src/routes/adminRoutes.js";
 import authRoutes from '../src/routes/authRoutes.js'
 import cookieParser from "cookie-parser";
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json()); // Body parser
@@ -29,6 +33,12 @@ app.use("/api/users", protect, userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+connectDB()
+.then(()=>{
+  app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+  );
+})
 // Start server
 
 // Connect to the database
