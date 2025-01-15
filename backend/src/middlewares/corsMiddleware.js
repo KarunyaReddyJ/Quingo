a2214c1d27ab7e2a99df0276a75c2fbff2c32277
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-15 23:24:35 +0530

    mvp

--- backend/src/middlewares/corsMiddleware.js
+++ backend/src/middlewares/corsMiddleware.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 export const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173", // Replace with your frontend URL
+  origin: process.env.CLIENT_URL || "https://quingo-d3dd.vercel.app/", // Replace with your frontend URL
   credentials: true,
 };
 
