62973cca75a3b2d32ef0435a4b9a28d933e5d08b
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-15 22:35:18 +0530

    mvp

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,6 +1,6 @@
-import { app } from "./app.js";
+import { app } from "../app.js";
 import dotenv from "dotenv";
-import connectDB from "./src/config/db.js";
+import connectDB from "../src/config/db.js";
 // Initialize environment variables
 dotenv.config();
 const PORT = process.env.PORT || 5000;
