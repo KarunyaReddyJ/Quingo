8441f22f16529225b204064b88664c33227f204d
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-16 01:44:36 +0530

    mvp

--- backend/src/middlewares/corsMiddleware.js
+++ backend/src/middlewares/corsMiddleware.js
@@ -1,10 +1,14 @@
 import cors from "cors";
 
 export const corsOptions = {
-  origin: process.env.CLIENT_URL || "https://quingo-d3dd.vercel.app/", // Replace with your frontend URL
+  origin: (origin, callback) => {
+    if (origin.includes('quingo') || !origin) {
+      callback(null, true);  
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, 
   credentials: true,
-  methods: ['GET','POST','OPTIONS'],
-  allowedHeaders: 'Content-Type, Authorization',
 };
 
 export const useCors = cors(corsOptions);
