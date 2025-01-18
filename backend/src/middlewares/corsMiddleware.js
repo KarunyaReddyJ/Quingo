75234a4b9067fabb30a7f6a3a22cdcfae295131e
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-18 14:16:38 +0530

    day 4

--- backend/src/middlewares/corsMiddleware.js
+++ backend/src/middlewares/corsMiddleware.js
@@ -1,21 +1,23 @@
 import cors from "cors";
 
-export const corsOptions = {
-  origin: (Origin,cb)=>{
-    console.log(cb.toString())
-    if(!Origin){
-      console.log('No origin')
-      return cb(null,true)
-    }
-    console.log('origin made request',Origin)
-    console.log('includes localhost ',Origin.includes('localhost'));
-    console.log('includes quingo ',Origin.includes('quingo'));
-    if(Origin.includes('quingo') || Origin.includes('localhost' || Origin.includes('https') ) ){
-      return cb(null,Origin)
-    }
-    return cb(new Error("Origin isnt right"))
-  }, 
-  credentials: true,
-};
+// export const corsOptions = {
+//   origin: (Origin,cb)=>{
+//     console.log(cb.toString())
+//     if(!Origin){
+//       console.log('No origin')
+//       return cb(null,true)
+//     }
+//     console.log('origin made request',Origin)
+//     console.log('includes localhost ',Origin.includes('localhost'));
+//     console.log('includes quingo ',Origin.includes('quingo'));
+//     if(Origin.includes('quingo') || Origin.includes('localhost' || Origin.includes('https') ) ){
+//       return cb(null,Origin)
+//     }
+//     return cb(new Error("Origin isnt right"))
+//   }, 
+//   credentials: true,
+// };
 
-export const useCors = cors(corsOptions);
+// export const useCors = cors(corsOptions);
+
+export const useCors=cors({origin:'*'})
