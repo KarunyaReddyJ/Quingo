8aadc2bf9b024134da2981740ed16acdcd017e34
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-25 16:23:03 +0530

    updated delete button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { useAuth } from "../hooks/useAuth";
+import Avatar from "react-avatar";
 
 const Header = () => {
-  
+  const {userDetails}=useAuth()
   return (
-    <div style={{margin:'0',padding:'0'}} >
+    <div style={{margin:'0',padding:'0',display:"flex",justifyContent:'space-between'}} >
       <h1 style={{ fontSize: "48px",textAlign:'left' }}>Quingo</h1>
-     
+      <div id="profile">
+        <Avatar name={userDetails.name}/>
+      </div>
     </div>
   );
 };
