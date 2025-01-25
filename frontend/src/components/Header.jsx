2fdb0af97df935ce0fa76b6c2ad46fb83f59cf05
KarunyaReddyJ <vpk8908@gmail.com>
2025-01-25 16:37:53 +0530

    updated delete button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,15 +2,16 @@ import { useAuth } from "../hooks/useAuth";
 import Avatar from "react-avatar";
 
 const Header = () => {
-  const {userDetails}=useAuth()
+  const {userDetails,loading}=useAuth()
   return (
     <div style={{margin:'0',padding:'0',display:"flex",justifyContent:'space-between'}} >
       <h1 style={{ fontSize: "48px",textAlign:'left' }}>Quingo</h1>
       
       {
+        loading ?(
         userDetails && <div id="profile">
         <Avatar name={userDetails.name}/>
-      </div>
+      </div>):<>Loading</>
       }
     </div>
   );
